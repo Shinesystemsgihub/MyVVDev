b8e13d9e41635df0aaebed9aac4be2037350a7cd
wilkesvb <vincentwilkes@MacBook-Pro-2.local>
2017-08-08 14:14:42 -0400

    8/8 I may have made a bad mistake

--- wp-content/themes/vogue-child/front-page.php
+++ wp-content/themes/vogue-child/front-page.php
@@ -5,26 +5,20 @@
  * Front Page Template
  * @package Vogue
  */
-get_template_part( 'header_front_page' ); ?>
-<hr style="height: 5px; font-size: 0; line-height: 0; background-color: #4AAF3E;"> 
-<div class="front-page-hero" >
-	<div class="front-page-content">
-		<h1 ><span style="font-weight: thin;">Groceries And Services<br/>Delivered Straight to Your Vacation</span><br/><br/></h1>  
-		<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?> " method="post" class="form-horizontal" >
-				<fieldset style="border: none;">
+get_template_part( 'header_front_page' ); 
 
-					<!-- Text input-->
-					<div class="form-group" >
-						<label for="zipcode" style="line-height: 18px;">Please enter the zipcode where you will be staying.</label>
-						<input id="zipcode" name="zipcode" type="text" placeholder="Vacation Zipcode" pattern="[0-9]{5}" title="Five digit zip code" size="5" class="form-control input-md" required >
-					</div>
+?>
 
-					<!-- Button -->
-					<div class="form-button" >
-								<input type="hidden" name="action" value="franchise_form" />
-								<input type="submit" id="zipcodeSubmit" name="zipcodeSubmit" class="btn btn-primary" value="GET STARTED" />
-					</div>
-				</fieldset>
-			</form>
-	</div>
-</div><?php get_template_part( 'footer_front_page' );
+<div style="margin: 50px 0px 100px;">
+
+<?php get_template_part( '/templates/titlebar' ); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php get_template_part( 'templates/contents/content', 'page' ); ?>
+
+<?php endwhile; // end of the loop. ?>
+
+</div>
+
+<?php get_template_part( 'footer_front_page' ); ?>
---
+++ wp-content/themes/vogue-child/how-it-works.php
@@ -0,0 +1,10 @@
+<div class="front-page-hero" >
+        
+    <div class="front-page-content">
+
+        <h1 ><span style="font-weight: thin;">Congratulations!</span><br/><br/>
+            You are staying in an area we service!</h1>
+    
+    </div>
+
+</div>
